fca2b4d549d415b314e546ef7ffb7880f2d4ce49
Joseph <joseph.coston@student.nmt.edu>
2019-01-27 01:00:47 -0700

    Added the DL1414 datasheet, a pinout diagram, and some Arduino code with hard-coded values.

---
+++ DL1414_display/DL1414_display.ino
@@ -0,0 +1,139 @@
+#include <Arduino.h>
+
+/* DL1414 character set definitions:
+ * Each character is defined as an 8 char long string of 1s and 0s that is NULL
+ * terminated. Each char in the string stands for the state of an input pin on
+ * the DL1414 chip and holds a binary value of either TRUE or FALSE. The
+ * leftmost char in the string represents the state of pin D6 on the DL1414 and
+ * the rightmost char represents the state of D0.
+ */
+/* SPECIAL CHARACTERS */
+#define Spc "0100000" /* [ ] white space (blank) */
+#define Exp "0100001" /* [!] exclamation point */
+#define Ats "1000000" /* [@] at sign */
+#define Pnd "0100011" /* [#] pound sign */
+#define Dlr "0100100" /* [$] dollar sign */
+#define Mod "0100101" /* [%] percent sign */
+#define Xor "1011110" /* [^] carrot */
+#define And "0100110" /* [&] ampersand */
+#define Ast "0101010" /* [*] asterisk (multiplication) */
+#define Opr "0101000" /* [(] open parentheses */
+#define Cpr "0101001" /* [)] closed parentheses */
+#define Mns "0101101" /* [-] minus sign (dash/hyphen) */
+#define Uln "1011111" /* [_] underscore */
+#define Pls "0101011" /* [+] plus sign */
+#define Eql "0111101" /* [=] equals sign */
+#define Obr "1011011" /* [[] open bracket */
+#define Obr "1011101" /* []] closed bracket */
+#define Bsh "1011100" /* [\] back slash  */
+#define Sem "0111011" /* [;] semicolon */
+#define Col "0111010" /* [:] colon */
+#define Apt "0100111" /* ['] apostrophe */
+#define Qtm "0100010" /* ["] quotation mark */
+#define Com "0101100" /* [,] comma */
+#define Lth "0111100" /* [<] less-than sign */
+#define Prd "0101110" /* [.] period (decimal) */
+#define Gth "0111110" /* [>] greater-than sign */
+#define Fsh "0101111" /* [/] forward slash (division) */
+#define Qst "0111111" /* [?] question mark */
+
+/* NUMBERS */
+#define No0 "0110000" /* [0] zero */
+#define No1 "0110001" /* [1] one */
+#define No2 "0110010" /* [2] two */
+#define No3 "0110011" /* [3] three */
+#define No4 "0110100" /* [4] four */
+#define No5 "0110101" /* [5] five */
+#define No6 "0110110" /* [6] six */
+#define No7 "0110111" /* [7] seven */
+#define No8 "0111000" /* [8] eight */
+#define No9 "0111001" /* [9] nine */
+
+/* LETTERS */
+#define _A_ "1000001" /* [A] capital a */
+#define _B_ "1000010" /* [B] capital b */
+#define _C_ "1000011" /* [C] capital c */
+#define _D_ "1000100" /* [D] capital d */
+#define _E_ "1000101" /* [E] capital e */
+#define _F_ "1000110" /* [F] capital f */
+#define _G_ "1000111" /* [G] capital g */
+#define _H_ "1001000" /* [H] capital h */
+#define _I_ "1001001" /* [I] capital i */
+#define _J_ "1001010" /* [J] capital j */
+#define _K_ "1001011" /* [K] capital k */
+#define _L_ "1001100" /* [L] capital l */
+#define _M_ "1001101" /* [M] capital m */
+#define _N_ "1001110" /* [N] capital n */
+#define _O_ "1001111" /* [O] capital o */
+#define _P_ "1010000" /* [P] capital p */
+#define _Q_ "1010001" /* [Q] capital q */
+#define _R_ "1010010" /* [R] capital r */
+#define _S_ "1010011" /* [S] capital s */
+#define _T_ "1010100" /* [T] capital t */
+#define _U_ "1010101" /* [U] capital u */
+#define _V_ "1010110" /* [V] capital v */
+#define _W_ "1010111" /* [W] capital w */
+#define _X_ "1011000" /* [X] capital x */
+#define _Y_ "1011001" /* [Y] capital y */
+#define _Z_ "1011010" /* [Z] capital z */
+
+/* digital I/O pins corresponding to each character select pin */
+#define D0 2
+#define D1 3
+#define D2 4
+#define D3 5
+#define D4 6
+#define D5 7
+#define D6 8
+
+/* cursor pins */
+#define A1 10
+#define A2 11
+
+/* write pins */
+#define WRL 9   /* left display */
+#define WRR 12  /* right display */
+
+
+
+void setup()
+{
+  /* set output pins */
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(WRL, OUTPUT);
+  pinMode(WRR, OUTPUT);
+  
+  /* set cursor pins to LOW */
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  
+  /* set write pins to HIGH */
+  digitalWrite(WRL, HIGH);
+  digitalWrite(WRR, HIGH);
+
+
+  /* set character select pins */
+  digitalWrite(D6, LOW);
+  digitalWrite(D5, HIGH);
+  digitalWrite(D4, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D1, LOW);
+  digitalWrite(D0, HIGH);
+  
+  
+  
+}
+
+void loop()
+{
+
+}
